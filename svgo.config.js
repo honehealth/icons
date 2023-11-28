6019464fed8ed4afab61fa4b26a8dc5e4c9d9e10
greym0uth <thejadenjack@gmail.com>
2023-11-28 10:20:04 -0600

    initial commit

---
+++ svgo.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false,
+          cleanupIDs: false,
+          prefixIds: false,
+          prefixClassNames: false,
+        },
+      },
+    },
+    'removeDimensions',
+    'removeDoctype',
+    'removeComments',
+    'removeMetadata',
+    'removeDesc',
+    'removeUnusedNS',
+    'removeEmptyContainers',
+    'removeEmptyAttrs',
+    'removeEmptyText',
+    'removeHiddenElems',
+    'removeUselessStrokeAndFill',
+    'removeUnknownsAndDefaults',
+    'removeUselessDefs',
+    'removeRasterImages',
+    'removeEditorsNSData',
+  ],
+};
